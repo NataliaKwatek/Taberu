98a1bdf659c4e0ce344e9ad0c9c76f361c22c1d1
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-09-26 19:22:42 +0200

    first commit - basic setup for app

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAB5dLVUuqptzA0w0YyIXlrqbKHhLwOPok",
+    authDomain: "taberu-8bcec.firebaseapp.com",
+    projectId: "taberu-8bcec",
+    storageBucket: "taberu-8bcec.appspot.com",
+    messagingSenderId: "984825847680",
+    appId: "1:984825847680:web:bcd2fe418deb15efb26b65"
+};
+
+const app = initializeApp(firebaseConfig);
