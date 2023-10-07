efe1f4a20dc4bfdad6bff1bdbc80aa56416449bd
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-10-07 12:47:17 +0200

    edit: new routing

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 export const HomePage = () => {
 
 const { logout } = useAuth();
+const { currentUser } = useAuth();
 
   return (
     <>
     <Toaster />
       <h2>Witaj na stronie Taberu</h2>
+      <h3>Zalogowany jako: {currentUser.email}</h3>
       <Link to="/display"><button>Wyświetl przepisy</button></Link>
       <Link to="/add"><button>Dodaj przepis</button></Link>
-      <Link to='/mealplan'><button>Stwórz plan posiłków</button></Link>
+      <Link to='/createplan'><button>Stwórz plan posiłków</button></Link>
+      <Link to='/displayplan'><button>Wyświetl plan posiłków</button></Link>
       <div>Jesteś zalogowany</div>
       <button onClick={logout}>Wyloguj</button>
     </>
