65e54c776adee9e6338a1ce7abe7122e0b505621
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-10-08 17:16:17 +0200

    fix: coreccted the way of adding nutritions into meal plan

--- src/components/MealPlan/CreateMealPlan/CreateMealPlan.jsx
+++ src/components/MealPlan/CreateMealPlan/CreateMealPlan.jsx
@@ -27,15 +27,7 @@ export const CreateMealPlan = () => {
     getRecipesToSelect();
   }, []);
 
-  const [mealPlan, setMealPlan] = useState({
-    date: "",
-    breakfast: "",
-    secondBreakfast: "",
-    dinner: "",
-    tea: "",
-    supper: "",
-    snack: "",
-  });
+  const mealPlanID = uuidv4();
 
   const [nutritionSummary, setNutritionSummary] = useState({
     calories: 0,
@@ -44,12 +36,67 @@ export const CreateMealPlan = () => {
     carbohydrates: 0,
   });
 
+  const [mealPlan, setMealPlan] = useState({
+    date: "",
+    breakfast: "",
+    secondBreakfast: "",
+    dinner: "",
+    tea: "",
+    supper: "",
+    snack: "",
+    nutritionSummary: {
+      calories: 0,
+      protein: 0,
+      fat: 0,
+      carbohydrates: 0,
+    },
+  });
+
+  const addMealPlanToDataBase = async (e) => {
+    e.preventDefault();
+
+    try {
+      const date = e.target.date.value;
+      const breakfast = e.target.breakfast.value;
+      const secondBreakfast = e.target.secondBreakfast.value;
+      const dinner = e.target.dinner.value;
+      const tea = e.target.tea.value;
+      const supper = e.target.supper.value;
+      const snack = e.target.snack.value;
+      const nutritionSummaryTemp = {
+        calories: nutritionSummary.calories,
+        protein: nutritionSummary.protein,
+        fat: nutritionSummary.fat,
+        carbohydrates: nutritionSummary.carbohydrates,
+      };
+
+      const newMealPlan = doc(db, "MealPlans", mealPlanID);
+
+      await setDoc(newMealPlan, {
+        date: date,
+        breakfast: breakfast,
+        secondBreakfast: secondBreakfast,
+        dinner: dinner,
+        tea: tea,
+        supper: supper,
+        snack: snack,
+        nutritionSummary: nutritionSummaryTemp,
+      });
+
+      console.log("Zapisano plan w bazie danych");
+      e.target.reset();
+      navigate("/displayplan");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   const handleSelect = (e) => {
     const newMealPlan = { ...mealPlan, [e.target.name]: e.target.value };
     setMealPlan(newMealPlan);
   };
 
-  useEffect(() => {
+const calculateNutritionSummary = () => {
     const newNutritionSummary = {
       calories: 0,
       protein: 0,
@@ -125,41 +172,11 @@ export const CreateMealPlan = () => {
     }
 
     setNutritionSummary(newNutritionSummary);
-  }, [mealPlan]);
-
-  const mealPlanID = uuidv4();
-
-  const addMealPlanToDataBase = async (e) => {
-    e.preventDefault();
-
-    try {
-      const date = e.target.date.value;
-      const breakfast = e.target.breakfast.value;
-      const secondBreakfast = e.target.secondBreakfast.value;
-      const dinner = e.target.dinner.value;
-      const tea = e.target.tea.value;
-      const supper = e.target.supper.value;
-      const snack = e.target.snack.value;
-
-      const newMealPlan = doc(db, "MealPlans", mealPlanID);
-
-      await setDoc(newMealPlan, {
-        date: date,
-        breakfast: breakfast,
-        secondBreakfast: secondBreakfast,
-        dinner: dinner,
-        tea: tea,
-        supper: supper,
-        snack: snack,
-      });
-
-      console.log("Zapisano plan w bazie danych");
-      e.target.reset();
-      navigate("/displayplan")
-    } catch (error) {
-      console.log(error.message);
-    }
   };
+
+  useEffect(() => {
+    calculateNutritionSummary();
+  }, [mealPlan]);
 
   return (
     <>
