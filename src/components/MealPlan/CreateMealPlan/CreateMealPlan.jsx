9663672ea4aa2503f965ddc4431cdd0f52eee7a6
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-10-10 13:29:21 +0200

    clean code in CreateMealPlan

--- src/components/MealPlan/CreateMealPlan/CreateMealPlan.jsx
+++ src/components/MealPlan/CreateMealPlan/CreateMealPlan.jsx
@@ -12,6 +12,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import calculateNutritionSummary from "../../../utils/calculateNutritionSummary";
 
 export const CreateMealPlan = () => {
   const [recipes, setRecipes] = useState([]);
@@ -96,86 +97,8 @@ export const CreateMealPlan = () => {
     setMealPlan(newMealPlan);
   };
 
-const calculateNutritionSummary = () => {
-    const newNutritionSummary = {
-      calories: 0,
-      protein: 0,
-      fat: 0,
-      carbohydrates: 0,
-    };
-
-    if (mealPlan.breakfast) {
-      const breakfast = recipes.find(
-        (recipe) => recipe.id === mealPlan.breakfast
-      );
-
-      if (breakfast) {
-        newNutritionSummary.calories += Number(breakfast.calories);
-        newNutritionSummary.protein += Number(breakfast.protein);
-        newNutritionSummary.fat += Number(breakfast.fat);
-        newNutritionSummary.carbohydrates += Number(breakfast.carbohydrates);
-      }
-    }
-
-    if (mealPlan.secondBreakfast) {
-      const secondBreakfast = recipes.find(
-        (recipe) => recipe.id === mealPlan.secondBreakfast
-      );
-      if (secondBreakfast) {
-        newNutritionSummary.calories += Number(secondBreakfast.calories);
-        newNutritionSummary.protein += Number(secondBreakfast.protein);
-        newNutritionSummary.fat += Number(secondBreakfast.fat);
-        newNutritionSummary.carbohydrates += Number(
-          secondBreakfast.carbohydrates
-        );
-      }
-    }
-
-    if (mealPlan.dinner) {
-      const dinner = recipes.find((recipe) => recipe.id === mealPlan.dinner);
-      if (dinner) {
-        newNutritionSummary.calories += Number(dinner.calories);
-        newNutritionSummary.protein += Number(dinner.protein);
-        newNutritionSummary.fat += Number(dinner.fat);
-        newNutritionSummary.carbohydrates += Number(dinner.carbohydrates);
-      }
-    }
-
-    if (mealPlan.tea) {
-      const tea = recipes.find((recipe) => recipe.id === mealPlan.tea);
-      if (tea) {
-        newNutritionSummary.calories += Number(tea.calories);
-        newNutritionSummary.protein += Number(tea.protein);
-        newNutritionSummary.fat += Number(tea.fat);
-        newNutritionSummary.carbohydrates += Number(tea.carbohydrates);
-      }
-    }
-
-    if (mealPlan.supper) {
-      const supper = recipes.find((recipe) => recipe.id === mealPlan.supper);
-      if (supper) {
-        newNutritionSummary.calories += Number(supper.calories);
-        newNutritionSummary.protein += Number(supper.protein);
-        newNutritionSummary.fat += Number(supper.fat);
-        newNutritionSummary.carbohydrates += Number(supper.carbohydrates);
-      }
-    }
-
-    if (mealPlan.snack) {
-      const snack = recipes.find((recipe) => recipe.id === mealPlan.snack);
-      if (snack) {
-        newNutritionSummary.calories += Number(snack.calories);
-        newNutritionSummary.protein += Number(snack.protein);
-        newNutritionSummary.fat += Number(snack.fat);
-        newNutritionSummary.carbohydrates += Number(snack.carbohydrates);
-      }
-    }
-
-    setNutritionSummary(newNutritionSummary);
-  };
-
   useEffect(() => {
-    calculateNutritionSummary();
+    calculateNutritionSummary(mealPlan, recipes, setNutritionSummary);
   }, [mealPlan]);
 
   return (
