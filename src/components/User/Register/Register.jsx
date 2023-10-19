5a80587b6daf4616e254b3a9f43ac2918498e3a9
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-10-19 19:19:55 +0200

    added styling and responsive desing to Register form

--- src/components/User/Register/Register.jsx
+++ src/components/User/Register/Register.jsx
@@ -7,7 +7,13 @@ import {
   showPassword,
   showPasswordConfirm,
 } from "../../../utils/ShowPasswords";
+import { Link } from "react-router-dom";
 import { toast } from "react-hot-toast";
+import webIcon from "../../../assets/icon-web-food.svg";
+import userIcon from "../../../assets/user.svg";
+import mailIcon from "../../../assets/mail.svg";
+import passwordIcon from "../../../assets/password.svg";
+import openPasswordIcon from "../../../assets/open-password.svg";
 
 
 export const Register = () => {
@@ -61,41 +67,72 @@ export const Register = () => {
 
   return (
     <>
+      <div className={styles.header}>
+            <div className={styles.logo}>
+              <Link to="/">
+                <img src={webIcon} />
+              </Link>
+            </div>
+          </div>
+
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
     
-      <h2>Zarejestruj się </h2>
+      <h1>Rejestracja</h1>
 
       <form onSubmit={handleRegister}>
-        <label htmlFor="name">Login</label>
-        <input type="text" id="name" name="name" />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Hasło</label>
+      
+      <div className={styles.input_box}>
+        <label htmlFor="name"></label>
+        <input type="text" id="name" name="name" placeholder="Login" required/>
+        <img src={userIcon} alt="ikona użytkownika" />
+        
+      </div>
+
+      <div className={styles.input_box}>
+        <label htmlFor="email"></label>
+        <input type="email" id="email" name="email" placeholder="Mail" required/>
+        <img src={mailIcon} alt="ikona maila" />
+</div>
+
+<div className={styles.input_box}>
+        <label htmlFor="password"></label>
         <input
           type="password"
           id="password"
           name="password"
           pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[&%$]).{8,}$"
           title="Twoje hasło musi zawierać 8 znaków, co najmniej 1 cyfrę, 1 znak specjalny i 1 dużą literę"
+          placeholder="Hasło"
+          required
         />
-        <input
-          type="checkbox"
-          id="show_password"
-          name="show_password"
-          onClick={showPassword}
-        />
-        Pokaż hasło
-        <label htmlFor="password_confirm">Potwierdź hasło</label>
-        <input type="password" id="password_confirm" name="password_confirm" />
-        <input
-          type="checkbox"
-          id="show_password_conf"
-          name="show_password_conf"
-          onClick={showPasswordConfirm}
-        />
-        Pokaż hasło
-        <button>Zarejestruj się</button>
+        <div onClick={showPassword} className={styles.pass_icons}>
+        <img src={passwordIcon} alt="ikona hasła" id="closed" className={styles.closed}/>
+        <img src={openPasswordIcon} alt="ikona pokazanego hasła" id="open_pass" className={styles.open_password_icon} />
+        </div>
+ 
+        </div>
+        <div className={styles.input_box}>
+        <label htmlFor="password_confirm"></label>
+        <input type="password" id="password_confirm" name="password_confirm" placeholder="Powtórz hasło" required />
+       
+        <div onClick={showPasswordConfirm} className={styles.pass_icons}>
+        <img src={passwordIcon} alt="ikona hasła" id="closed_conf" className={styles.closed}/>
+        <img src={openPasswordIcon} id="open_pass_conf" alt="ikona pokazanego hasła" className={styles.open_password_icon} />
+        </div>
+  
+        </div>
+        <button type="submit" className={styles.register_button}>Zarejestruj się</button>
+
+        <div className={styles.login_link}>
+                  <p>
+                    Masz już konto? <Link to="/login">Zaloguj się</Link>
+                  </p>
+                </div>
+
       </form>
-     
+     </div>
+      </div>
     </>
   );
 };
