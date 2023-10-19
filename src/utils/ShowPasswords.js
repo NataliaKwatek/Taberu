e7ad911cfeb1a9f7ef84dab621621a02c2d104f0
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-10-19 19:20:40 +0200

    change: showing/hiding password via icons

--- src/utils/ShowPasswords.js
+++ src/utils/ShowPasswords.js
@@ -1,18 +1,30 @@
 const showPassword = () => {
     const showPass = document.getElementById('password');
+    const closedIcon = document.getElementById('closed');
+    const openIcon = document.getElementById('open_pass');
     if (showPass.type === 'password') {
         showPass.type = 'text';
+        closedIcon.style.display = 'none';
+        openIcon.style.display = 'block';
     } else {
         showPass.type = 'password';
+        closedIcon.style.display = 'block';
+        openIcon.style.display = 'none';
     }
 }
 
 const showPasswordConfirm = () => {
     const showPassConf = document.getElementById('password_confirm');
+    const closedIconConf = document.getElementById('closed_conf');
+    const openIconConf = document.getElementById('open_pass_conf');
     if (showPassConf.type === 'password') {
         showPassConf.type = 'text';
+        closedIconConf.style.display = 'none';
+        openIconConf.style.display = 'block';
     } else {
         showPassConf.type = 'password';
+        closedIconConf.style.display = 'block';
+        openIconConf.style.display = 'none';
     }
 }
 
