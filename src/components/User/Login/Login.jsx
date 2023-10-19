1969f0351d3a8ab3308340f2295c76918c1e06be
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-10-19 16:31:55 +0200

    add styling and responsive design to login page

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -2,6 +2,10 @@ import { Navigate, Link } from "react-router-dom";
 import styles from "./Login.module.css";
 import useAuth from "../../../context/AuthContext";
 import { showPassword } from "../../../utils/ShowPasswords";
+import userIcon from "../../../assets/user.svg";
+import passwordIcon from "../../../assets/password.svg";
+import mailIcon from "../../../assets/mail.svg";
+import webIcon from "../../../assets/icon-web-food.svg";
 
 export const Login = () => {
   const { currentUser, login } = useAuth();
@@ -21,24 +25,64 @@ export const Login = () => {
 
   return (
     <>
-
       {!currentUser ? (
         <>
-        <h2>Zaloguj się</h2>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" required />
-          <label htmlFor="password">Hasło</label>
-          <input type="password" name="password" id="password" required />
-          <input type="checkbox" onClick={showPassword} />
-          Pokaż hasło
-          <button>Zaloguj się</button>
-        </form>
-        Nie pamiętasz hasła? 
-        <Link to="/passwordreminder"><button>Zresetuj hasło</button></Link>
+          <div className={styles.header}>
+            <div className={styles.logo}>
+              <Link to="/">
+                <img src={webIcon} />
+              </Link>
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.wrapper}>
+              <h1>Logowanie</h1>
+              <form onSubmit={handleLogin}>
+                <div className={styles.input_box}>
+                  <label htmlFor="email"></label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Mail"
+                    required
+                  />
+                  <img src={mailIcon} alt="ikona maila" />
+                </div>
+                <div className={styles.input_box}>
+                  <label htmlFor="password"></label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Hasło"
+                    required
+                  />
+                  <img src={passwordIcon} alt="ikona hasła" />
+                </div>
+
+                <div className={styles.show_forgot}>
+                  <label>
+                    <input type="checkbox" onClick={showPassword} />
+                    Pokaż hasło
+                  </label>
+                  <Link to="/passwordreminder">Przypomnij hasło</Link>
+                </div>
+
+                <button type="submit" className={styles.login_button}>
+                  Zaloguj się
+                </button>
+                <div className={styles.register_link}>
+                  <p>
+                    Nie masz konta? <Link to="/register">Zarejestruj się</Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
         </>
       ) : (
-        <Navigate to='/home' />
+        <Navigate to="/home" />
       )}
     </>
   );
