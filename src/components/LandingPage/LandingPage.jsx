7b9042aa0ec9389c0a7612b8e3cff2a942c35446
Natalia Kwatek <kwatek.natalia@gmail.com>
2023-10-24 13:33:44 +0200

    fix: fixed error with routing after being login in

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useState } from "react";
+import useAuth from "../../context/AuthContext";
 import styles from "./LandingPage.module.css";
 import contentPhoto from "../../assets/asian-bowl-food-svgrepo-com.svg";
 import menuIcon from "../../assets/menu-icon.svg";
@@ -10,71 +11,82 @@ import GhIcon from "../../assets/github-icon.svg";
 // import { Navigation } from "../Navigation/Navigation";
 
 export const LandingPage = () => {
+  const { currentUser } = useAuth();
 
-const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-const toggleMenu = () => {
-  console.log("click");
-  setIsOpen((open) => !open);
-}
+  const toggleMenu = () => {
+    setIsOpen((open) => !open);
+  };
 
   return (
     <>
-      <header>
-        <Link to="/home" className={styles.logo}>
-           <img src={appIcon} />
-        </Link>
-       
-        <div className={styles.menuToggle} onClick={toggleMenu}>
-          <img className={styles.menu_icon} src={menuIcon} alt="ikona menu" />
-        </div>
+      {!currentUser ? (
+        <>
+          <header>
+            <Link to="/home" className={styles.logo}>
+              <img src={appIcon} />
+            </Link>
 
-        <ul className={(isOpen ? styles.open : styles.navigation)}>
-          <li>
-            <Link to="/login">Zaloguj się</Link>
-          </li>
-          <li>
-            <a href="#">Kontakt</a>
-          </li>
-          <li>
-            <a href="#">Credits</a>
-          </li>
-        </ul>
-       
+            <div className={styles.menuToggle} onClick={toggleMenu}>
+              <img
+                className={styles.menu_icon}
+                src={menuIcon}
+                alt="ikona menu"
+              />
+            </div>
 
-       
+            <ul className={isOpen ? styles.open : styles.navigation}>
+              <li>
+                <Link to="/login">Zaloguj się</Link>
+              </li>
+              <li>
+                <a href="#">Kontakt</a>
+              </li>
+              <li>
+                <a href="#">Credits</a>
+              </li>
+            </ul>
+          </header>
 
-      </header>
+          <section className={styles.hero}>
+            <div className={styles.hero_text}>
+              <h2>
+                Witaj w aplikacji do zarządzania przepisami oraz planowania
+                posiłków
+              </h2>
+              <h1>taberu</h1>
+              <p>
+                Taberu to japoński czasownik oznaczający &quot;jeść&quot; a
+                aplikacja <i>taberu</i> ma na celu ułatwienie planowania
+                posiłków. Dzięki naszej apce już nigdy nie będziesz mieć
+                problemu z pytaniem &quot;co dzisiaj na obiad&quot; a zapisane
+                rolki czy posty z inspiracjami wreszcie ujrzą światło dzienne
+                i... Twój talerz.
+              </p>
 
-      <section className={styles.hero}>
-        <div className={styles.hero_text}>
-          <h2>
-            Witaj w aplikacji do zarządzania przepisami oraz planowania posiłków
-          </h2>
-          <h1>taberu</h1>
-          <p>
-            Taberu to japoński czasownik oznaczający &quot;jeść&quot; a
-            aplikacja <i>taberu</i> ma na celu ułatwienie Ci codzienne
-            planowanie posiłków. Dzięki naszej apce już nigdy nie będziesz mieć
-            problemu z pytaniem &quot;co dzisiaj na obiad&quot; a zapisane rolki
-            czy posty z inspiracjami wreszcie ujrzą światło dzienne i... Twój talerz.
-          </p>
+              <Link to="/register">
+                <button className={styles.cta_button}>Chcę spróbować</button>{" "}
+              </Link>
+            </div>
+            <div className={styles.hero_image}>
+              <img src={contentPhoto} />
+            </div>
+          </section>
 
-          <Link to="/register">
-            <button className={styles.cta_button}>Chcę spróbować</button>{" "}
-          </Link>
+          <div className={styles.social_icons}>
+            <a href="#">
+              <img src={LiIcon} />
+            </a>
+            <a href="#">
+              {" "}
+              <img src={GhIcon} />
+            </a>
           </div>
-          <div className={styles.hero_image}>
-          <img src={contentPhoto} />
-          </div>
-      </section>
-
-      <div className={styles.social_icons}>
-        <a href="#"><img src={LiIcon} /></a>
-        <a href="#"> <img src={GhIcon} /></a>
-      </div>
-
-      {/* <Footer /> */}
+        </>
+      ) : (
+        <Navigate to="/home" />
+      )}
     </>
   );
 };
